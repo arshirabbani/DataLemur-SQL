e5b470165b4eb165ca71e2814a25152d4d5bb8c1
Arshi Rabbani <91269163+arshirabbani@users.noreply.github.com>
2022-11-09 13:56:13 +0530

    Update 06_easy_ebay_highest_number_of_products.sql

--- 01_SCRIPTS/Easy/06_easy_ebay_highest_number_of_products.sql
+++ 01_SCRIPTS/Easy/06_easy_ebay_highest_number_of_products.sql
@@ -1,22 +1,16 @@
 -- highest number of products --
 
-SELECT
-    user_id,
-    product_sum
-FROM
-(
-    SELECT
-        user_id,
-        COUNT(product_id) AS product_sum,
-        SUM(spend) AS total_spend
-    FROM
-        user_transactions
-    GROUP BY
-        user_id
-) AS temp_tbl
-WHERE
-    total_spend >= 1000
+SELECT 
+  user_id, 
+  count(product_id) as prod_num 
+FROM 
+  user_transactions 
+GROUP BY 
+  user_id 
+HAVING 
+  sum(spend) >= 1000 
 ORDER BY
-    product_sum DESC,
-    total_spend DESC
-LIMIT 3;
+  prod_num desc, 
+  sum(spend) desc 
+limit 
+  3;
