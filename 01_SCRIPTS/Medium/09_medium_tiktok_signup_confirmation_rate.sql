1db617296ba9265ea7dab5e7ce35574bc72deffd
Arshi Rabbani <91269163+arshirabbani@users.noreply.github.com>
2023-09-26 17:55:32 +0200

    Update 09_medium_tiktok_signup_confirmation_rate.sql

--- 01_SCRIPTS/Medium/09_medium_tiktok_signup_confirmation_rate.sql
+++ 01_SCRIPTS/Medium/09_medium_tiktok_signup_confirmation_rate.sql
@@ -1,21 +1,7 @@
 -- signup confirmation rate --
-
-WITH signup_info AS
-(
-    SELECT
-        e.email_id,
-        e.user_id,
-        t.signup_action
-    FROM
-        emails AS e
-        LEFT JOIN texts AS t
-        ON e.email_id = t.email_id
-)
-
-SELECT
-    ROUND(
-        (SELECT COUNT(DISTINCT email_id) FROM signup_info WHERE signup_action = 'Confirmed')::NUMERIC/
-        COUNT(DISTINCT email_id)::NUMERIC, 2
-    ) AS confirm_rate
-FROM
-    signup_info;
+SELECT 
+    ROUND((1.0 * SUM(CASE WHEN signup_action = 'Confirmed' THEN 1 ELSE 0 END))/ COUNT(*),2)
+FROM emails
+LEFT JOIN texts
+ON texts.email_id = emails.email_id
+WHERE signup_action IS NOT NULL;
