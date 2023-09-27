511d665f5b796fe371a9f43aef2b4dfb74e1d891
Arshi Rabbani <91269163+arshirabbani@users.noreply.github.com>
2023-09-27 12:00:37 +0200

    Update 03_easy_nyt_laptop_vs_mobile_viewership.sql

--- 01_SCRIPTS/Easy/03_easy_nyt_laptop_vs_mobile_viewership.sql
+++ 01_SCRIPTS/Easy/03_easy_nyt_laptop_vs_mobile_viewership.sql
@@ -1,12 +1,6 @@
--- laptop vs. mobile viewership --
+--Laptop vs. Mobile Viewership--
 
-SELECT 
-  sum(
-    case when device_type = 'laptop' then 1 else 0 end
-  ) as laptop_views, 
-  sum(
-    case when device_type in('phone', 'tablet') then 1 else 0 end
-  ) as mobile_views 
-FROM 
-  viewership
-
+SELECT
+sum(case when device_type = 'laptop' then 1 else 0 end) as laptop_views,
+sum(case when device_type in('phone','tablet') then 1 else 0 end) as mobile_views
+from viewership
