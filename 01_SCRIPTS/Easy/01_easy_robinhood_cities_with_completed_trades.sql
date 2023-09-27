109abf48512c47c05cf5e18cf58e33d6cedfb30e
Arshi Rabbani <91269163+arshirabbani@users.noreply.github.com>
2023-09-27 11:51:17 +0200

    Update 01_easy_robinhood_cities_with_completed_trades.sql

--- 01_SCRIPTS/Easy/01_easy_robinhood_cities_with_completed_trades.sql
+++ 01_SCRIPTS/Easy/01_easy_robinhood_cities_with_completed_trades.sql
@@ -1,16 +1,12 @@
 -- cities with completed trades -- 
 
-select 
-  u.city, 
-  count(order_id) as total_orders 
-from 
-  trades t 
-  inner join users u on t.user_id = u.user_id 
-where 
-  t.status = 'Completed' 
-group by 
-  city 
-order by 
-  total_orders desc 
-limit 
-  3;
+SELECT city, 
+count( order_id) as completed_order
+from trades t
+inner join users u   
+on u.user_id = t.user_id
+where status = 'Completed'
+group by city
+order by completed_order DESC
+limit 3;
+
