c6c0ba0d2d3c124e215a79163de819b0ea9de8f9
Arshi Rabbani <91269163+arshirabbani@users.noreply.github.com>
2023-09-27 12:36:53 +0200

    Update 05_easy_microsoft_teams_power_users.sql

--- 01_SCRIPTS/Easy/05_easy_microsoft_teams_power_users.sql
+++ 01_SCRIPTS/Easy/05_easy_microsoft_teams_power_users.sql
@@ -1,15 +1,10 @@
--- microsoft teams power users --
+--Teams Power Users--
 
-SELECT
-    sender_id,
-    COUNT(message_id) AS message_count
-FROM
-    messages
-WHERE
-    DATE_PART('year', sent_date) = 2022 AND
-    DATE_PART('month', sent_date) = 8
-GROUP BY
-    sender_id
-ORDER BY
-    message_count DESC
-LIMIT 2; 
+select sender_id,
+count(message_id) as number_of_message
+from messages
+where DATE_PART('year', sent_date) = 2022
+and DATE_PART('month', sent_date) = 8
+group by sender_id
+order by number_of_message DESC
+limit 2;
